2cf4909cb1727da6d4c1a16f8cda4d4785c94b28
Isaac Mosqueda <69749643+MOSQUEDACORDOVA@users.noreply.github.com>
2025-03-11 14:06:40 -0400

    Initial

---
+++ rewards-sistema-nube.js
@@ -0,0 +1,39 @@
+// Cargar el archivo CSS
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://firebasestorage.googleapis.com/v0/b/pruebasy-scripts-genera-97j72j.firebasestorage.app/o/styles.css?alt=media&token=8b7cdc66-ff71-4f3f-b128-e6a222876a7a'; // URL del archivo CSS en la CDN
+document.head.appendChild(link);
+
+// Crear el botón flotante
+const button = document.createElement('button');
+button.id = 'floatingButton';
+button.innerText = 'Externo 2';
+button.style.position = 'fixed';
+button.style.bottom = '20px';
+button.style.left = '20px';
+button.style.padding = '10px 20px';
+button.style.backgroundColor = '#007BFF';
+button.style.color = '#FFF';
+button.style.border = 'none';
+button.style.borderRadius = '5px';
+button.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.3)';
+button.style.cursor = 'pointer';
+
+// Agregar el botón al cuerpo del documento
+document.body.appendChild(button);
+
+// Cargar el contenido de la ventana emergente desde un archivo HTML
+fetch('https://mosquedacordova.com', { mode: 'cors' }) // URL del archivo HTML en la CDN
+  .then(response => response.text())
+  .then(html => {
+    document.body.insertAdjacentHTML('beforeend', html);
+
+    // Obtener referencia al popup
+    const popup = document.getElementById('popup');
+
+    // Mostrar la ventana emergente al hacer clic en el botón
+    button.addEventListener('click', () => {
+      popup.style.display = 'block';
+    });
+  })
+  .catch(error => console.error('Error al cargar el HTML:', error));
