98aede01b1ef7c96c185971c9d46e887d4369fac
Isaac Mosqueda <69749643+MOSQUEDACORDOVA@users.noreply.github.com>
2025-03-16 16:22:11 -0400

    Initial

--- public/rewards-sistema-nube.js
+++ public/rewards-sistema-nube.js
@@ -1,14 +1,13 @@
 // Cargar el archivo CSS
 const link = document.createElement('link');
 link.rel = 'stylesheet';
-link.href = 'https://pruebasy-scripts-genera-97j72j.web.app/styles.css?v=3'; // URL del archivo CSS en la CDN
+link.href = 'https://pruebasy-scripts-genera-97j72j.web.app/styles.css?v=5'; // URL del archivo CSS en la CDN
 //link.href = 'styles.css?v=3'; // URL del archivo CSS en la CDN
 document.head.appendChild(link);
 
 // Crear el botón flotante
 const button = document.createElement('button');
 button.id = 'floatingButton';
-button.innerText = 'btn';
 
 // Agregar el botón al cuerpo del documento
 document.body.appendChild(button);
@@ -16,10 +15,19 @@ document.body.appendChild(button);
 // Crear el iframe para la ventana emergente
 const iframe = document.createElement('iframe');
 iframe.id = 'popupIframe';
-iframe.src = 'https://popup-sistema-nube.web.app/'; // URL del archivo HTML en la CDN
+iframe.src = 'https://sistemanube.net/popUpInicio'; // URL del archivo HTML en la CDN
 document.body.appendChild(iframe);
 
 // Mostrar la ventana emergente al hacer clic en el botón
 button.addEventListener('click', () => {
   iframe.style.display = 'block';
 });
+
+(function () {
+  if (LS.customer) {
+      console.log("Cliente autenticado con ID:", LS.customer);
+      iframe.src = 'https://sistemanube.net/popUpMyAccount'; // URL del archivo HTML para clientes autenticados
+  } else {
+      console.log("No hay un cliente autenticado.");
+  }
+})();
